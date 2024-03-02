3a61eb33e91b9f0255da8947ad22637bb59832ac
nowaveosu <nowaveosu@gmail.com>
2024-03-02 14:55:23 +0900

    [Add] 글읽기 변경

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,7 +1,12 @@
-export default function Read(props){
+// 데이터를 읽어서 출력할뿐 => 서버컴포넌트
+// 사용자와 상호작용한다 => 클라이언트컴포넌트 
+export default async function Read(props){
+    const resp = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+    const topic = await resp.json();
     return(
         <>
-        <h2>Read</h2>
+        <h2>{topic.title}</h2>
+        {topic.body}
         parameters : {props.params.id}
         </>
     )
