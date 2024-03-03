e5aa07e2e84bf983d10a471328b742071927e547
nowaveosu <nowaveosu@gmail.com>
2024-03-03 11:29:14 +0900

    [Add] eventDefault는 url변경을 방지

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,5 +1,32 @@
+"use client"
 export default function Create(){
     return(
-        <>Create!</>
+        <form onSubmit={(e) => {
+            e.preventDefault(); //url이 멋대로 바뀌는걸 방지
+            const title = e.target.title;
+            const body = e.target.body.value;
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({title, body})
+            }
+            fetch(`http://localhost:9999/topics`, options)
+                .then(res=>res.json())
+                .then(result =>{
+                    console.log(result);
+                })
+        }}>
+            <p>
+                <input type="text" name="title" placeholder="title"/>
+            </p>
+            <p>
+                <textarea name="body" placeholder="body"></textarea>
+            </p>
+            <p>
+                <input type="submit" value="create" />
+            </p>
+        </form>
     )
 }
