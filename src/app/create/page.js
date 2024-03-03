a50f53c0c783ff6f70ccca65bbe96d6adb70d121
nowaveosu <nowaveosu@gmail.com>
2024-03-03 11:48:10 +0900

    [Add] 글생성 완료

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,11 +1,15 @@
 "use client"
+
+import { useRouter } from "next/navigation";
+
 export default function Create(){
+    const router = useRouter();
     return(
         <form onSubmit={(e) => {
             e.preventDefault(); //url이 멋대로 바뀌는걸 방지
-            const title = e.target.title;
+            const title = e.target.title.value;
             const body = e.target.body.value;
-            const options = {
+            const options = {  //서버로 데이터 추가할때 필요한 옵션
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -16,6 +20,8 @@ export default function Create(){
                 .then(res=>res.json())
                 .then(result =>{
                     console.log(result);
+                    const lastid = result.id;
+                    router.push(`/read/${lastid}`)
                 })
         }}>
             <p>
